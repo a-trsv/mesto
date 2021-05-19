9253ba10665241117f48f4b2b7f5527e4ec64563
Aleksi Tarasov <alexey2g@yandex.ru>
2021-05-19 19:57:54 +0300

    fix: added rimraf

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: { main: './src/pages/index.js' },
@@ -43,6 +44,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(), 
     ]
 }
